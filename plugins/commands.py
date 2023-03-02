829cddf52217b2e83fc8716c2b7f4ce65aaa443f
CrezyDeveloper <121280660+crezydeveloperr@users.noreply.github.com>
2023-03-02 10:55:54 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -38,7 +38,7 @@ async def mdisk_handler(_, event: Message):
             ],
 
              [InlineKeyboardButton("TeraBox", callback_data="Terabox_msg"),
-             InlineKeyboardButton('Watch Video', url='https://t.me/CyniteBackup/17')
+             InlineKeyboardButton('Watch Video', url='https://t.me/dulink_file_open')
              ]
         ])
     )
@@ -68,7 +68,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url=f'https://t.me/Cynitesupport')
+            InlineKeyboardButton('Support', url=f'https://t.me/TG_TR_support_bot')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
