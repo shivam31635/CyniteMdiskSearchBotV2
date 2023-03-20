86ef5607cc19f9b53fd3769d7d0e57425f9ca0bc
CrezyDeveloper <121280660+crezydeveloperr@users.noreply.github.com>
2023-03-20 20:07:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/crezydeveloperr/MDISK-SEARCH-BOT-2.git /MDISK-SEARCH-BOT-2
-cd /MDISK-SEARCH-BOT-2
+git clone https://github.com/crezydeveloperr/CyniteMdiskSearchBotV2.git /CyniteMdiskSearchBotV2
+cd /CyniteMdiskSearchBotV2
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
